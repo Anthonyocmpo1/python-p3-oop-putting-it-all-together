633ce93f3a4e0c50dbac472473b6bf6b71414f52
Anthony mwaura <antony.mwaura@student.moringaschool.comc0deanthony@anthony-HP-EliteBook-840-G5:~/f11$ git config --global user.email antony.mwaura@student.moringaschool.com>
2024-12-02 09:38:47 +0300

    add

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        """
+        Initialize a Shoe instance with a brand and size.
+        """
+        self.brand = brand
+        self._size = None  # Use a private attribute for validation
+        self.size = size  # Trigger the setter for initial validation
+        self.condition = "New"  # Default condition is "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        """
+        Repairs the shoe and sets its condition to 'New'.
+        """
+        self.condition = "New"
+        print("Your shoe is as good as new!")
